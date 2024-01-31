a8e45d5e003332363f87e71a7938047d0ae5a47f
hashrock <hashrock@users.noreply.github.com>
2024-02-01 02:18:56 +0900

    udpate

--- lv1/routes/index.tsx
+++ lv1/routes/index.tsx
@@ -1,15 +1,18 @@
 export default function Home() {
   return (
     <div>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link href="https://fonts.googleapis.com/css2?family=Yuji+Syuku&display=swap" rel="stylesheet"></link>
       <style>
         {`
         h1 {
-          color: red;
+          font-family: 'Yuji Syuku', sans-serif;
+          font-size: 50px;
         }
       `}
       </style>
-
-      <h1>Hello, World</h1>
+      <h1>春までに3kg痩せる</h1>
     </div>
   );
 }
