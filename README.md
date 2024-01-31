0c6cbe5773a47d8c8ef6373c8e1adeefb4081e84
hashrock <hashrock@users.noreply.github.com>
2024-02-01 02:14:37 +0900

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+WIP
+
+https://scrapbox.io/sushitecture/Fresh_+_Deno_KV_%E3%83%8F%E3%83%B3%E3%82%BA%E3%82%AA%E3%83%B3
---
+++ lv1/routes/index.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div>
+      <style>
+        {`
+        h1 {
+          color: red;
+        }
+      `}
+      </style>
+
+      <h1>Hello, World</h1>
+    </div>
+  );
+}
