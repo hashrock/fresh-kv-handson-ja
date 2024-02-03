7dbbf80c63382d70714b6ee4406538bcfbb380cd
hashrock <hashrock@users.noreply.github.com>
2024-02-04 01:21:34 +0900

    add basic auth

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,3 +1,9 @@
 import { defineConfig } from "$fresh/server.ts";
+import basicAuthPlugin from "https://deno.land/x/hashrock_fresh_plugins@1.0.0/basic.ts";
 
-export default defineConfig({});
+
+export default defineConfig({
+  plugins: [
+    basicAuthPlugin("/update")
+  ]
+});
